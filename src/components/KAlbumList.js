0ccb7bbb82b34f4c5693232d4942537bd70057cd
hmh <215795583@qq.com>
2017-04-28 20:29:13 +0800

    更新视频列表和节目列表

--- src/components/KAlbumList.js
+++ src/components/KAlbumList.js
@@ -1,67 +1,116 @@
 let template = 
 `
-<div class="tablist vediolist">
-  <table><!--table里面显示5条视频信息-->
-    <tbody><tr class="title">
-      <td class="tb01">节目</td>
-      <td class="tb02">节目</td>
-      <td class="tb02">节目</td>
-      <td class="tb02">节目</td>
+<table class="table table-bordered"> 
+  <thead>
+    <tr class="text-center">
+      <td><input type="checkbox" :attr="{checked: allChecked}" :click="checkAll"/></td>
+      <td>视频</td>
+      <td>视频数</td>
+      <td>热度</td>
+      <td>操作</td>
     </tr>
-    <tr class="list">
-      <td class="tb01">
-        <label class="checkbox" for="checkbox2">
-          <span class="icon-to-fade"></span>
-                <input type="checkbox" value="" id="checkbox2">
-            </label>
-            <img src="img/videolist.png">
-          <span class="listtitle" title="2016年度电影TOP50">2016年度电影TOP50</span>
-          <span class="listdate">2017-03-2315:23</span>
+  </thead>
+  <tbody>
+    <tr :for="(i, v) in list">
+      <td>
+        <input type="checkbox" :attr="{checked: v.checked}" :click="checkOne($event, v)"/>
       </td>
-      <td class="tb02">
-        3
+      <td style="width: 60%">
+        <div class="media">
+          <div class="media-left">
+            <a href="javascript:void(0)">
+              <img style="width: 100px; height: 138px;" :attr="{src: v.poster}">
+            </a>
+          </div>
+          <div class="media-body text-left">
+            <dl>
+              <dt :attr="{title: v.title}">{{v.title}}</dt>
+              <dd>{{v.create_time}}</dd>
+            </dl>
+          </div>
+        </div>
       </td>
-      <td class="tb02">
-        <p class="mar_top60"><i class="icon icon_redio"></i><span class="redtext">36</span></p>
+      <td>
+        <p>{{v.num}}</p>
       </td>
-      <td class="tb02">               
-        <p class="mar_top40"><a href="">添加视频</a></p>
-        <p><a href="">管理节目</a></p>
-        <p><a href="" class="">删除</a></p>
+      <td>
+        <p>
+          <i class="icon icon_redio"></i>
+          <span class="redtext">{{v.plays}}</span>
+        </p>
+        <p>
+          <i class="icon icon_redio"></i>
+          <span class="redtext">{{v.comments}}</span>
+        </p>
+      </td>
+      <td>
+        <p><a href="javascript:void(0)" :click="add">添加视频</a></p>
+        <p><a href="javascript:void(0)" :click="manage">管理节目</a></p>
+        <p><a href="javascript:void(0)" :click="remove">删除</a></p>
       </td>
     </tr>
-    <tr class="list">
-      <td class="tb01">
-        <label class="checkbox" for="checkbox2">
-          <span class="icon-to-fade"></span>
-                <input type="checkbox" value="" id="checkbox2">
-            </label>
-            <img src="img/videolist.png">
-          <span class="listtitle" title="2016年度电影TOP50">2016年度电影TOP50</span>
-          <span class="listdate">2017-03-2315:23</span>
-      </td>
-      <td class="tb02">
-        3
-      </td>
-      <td class="tb02">
-        <p class="mar_top60"><i class="icon icon_redio"></i><span class="redtext">36</span></p>
-      </td>
-      <td class="tb02">               
-        <p class="mar_top40"><a href="">添加视频</a></p>
-        <p><a href="">管理节目</a></p>
-        <p><a href="" class="">删除</a></p>
-      </td>
-    </tr>                   
-  </tbody></table>
-</div>
+  </tbody>
+</table>
 `
 
 export default {
   name: 'k-album-list',
   template,
   data () {
-    return {}
+    return {
+      allChecked: false,
+      list: [{
+        id: '1',
+        title: '2016年度电影TOP50',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493392486834&di=7a83985763d60a7827e4d98c14002e83&imgtype=0&src=http%3A%2F%2Fdynamic-image.yesky.com%2F600x-%2FuploadImages%2Fupload%2F20141120%2Fzhnygl4v2ckjpg.jpg',
+        create_time: '2017-03-23 15:23',
+        num: 50,
+        plays: '12312',
+        checked: false
+      }, {
+        id: '3',
+        title: '电视节目',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493392486834&di=7a83985763d60a7827e4d98c14002e83&imgtype=0&src=http%3A%2F%2Fdynamic-image.yesky.com%2F600x-%2FuploadImages%2Fupload%2F20141120%2Fzhnygl4v2ckjpg.jpg',
+        create_time: '2017-03-23 15:23',
+        num: 30,
+        plays: '12312',
+        checked: false
+      }, {
+        id: '2',
+        title: '其他',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493392486834&di=7a83985763d60a7827e4d98c14002e83&imgtype=0&src=http%3A%2F%2Fdynamic-image.yesky.com%2F600x-%2FuploadImages%2Fupload%2F20141120%2Fzhnygl4v2ckjpg.jpg',
+        create_time: '2017-03-23 15:23',
+        num: 0,
+        plays: '12312',
+        checked: false
+      }]
+    }
   },
-  computed: {
+  methods: {
+    checkAll (evt) {
+      let checked = this.allChecked = !this.allChecked
+      this.list.forEach(function(v) {
+        v.checked = checked
+      })
+    },
+    checkOne (evt, video) {
+      let checked = video.checked = !video.checked
+      if (checked === false) {
+        this.allChecked = false
+      } else {//avalon已经为数组添加了ecma262v5的一些新方法
+        this.allChecked = this.list.every(function (v) {
+          return v.checked
+        })
+      }
+    },
+    add () {
+
+    },
+    manage () {
+
+    },
+    remove () {
+
+    }
   }
 }
--- src/components/KVideoList.js
+++ src/components/KVideoList.js
@@ -1,67 +1,126 @@
 let template = 
 `
-<div class="tablist vediolist">
-  <table><!--table里面显示5条视频信息-->
-    <tbody><tr class="title">
-      <td class="tb01">视频</td>
-      <td class="tb02">视频数</td>
-      <td class="tb02">视频</td>
-      <td class="tb02">视频</td>
+<table class="table table-bordered">
+  <thead>
+    <tr class="text-center">
+      <td><input type="checkbox" :attr="{checked: allChecked}" :click="checkAll"/></td>
+      <td>视频</td>
+      <td>状态</td>
+      <td>热度</td>
+      <td>操作</td>
     </tr>
-    <tr class="list">
-      <td class="tb01">
-        <label class="checkbox" for="checkbox2">
-          <span class="icon-to-fade"></span>
-                <input type="checkbox" value="" id="checkbox2">
-            </label>
-            <img src="img/videolist.png">
-          <span class="listtitle" title="2016年度电影TOP50">2016年度电影TOP50</span>
-          <span class="listdate">2017-03-2315:23</span>
+  </thead>
+  <tbody>
+    <tr :for="(i, v) in list">
+      <td>
+        <input type="checkbox" :attr="{checked: v.checked}" :click="checkOne($event, v)"/>
       </td>
-      <td class="tb02">
-        3
+      <td style="width: 60%">
+        <div class="media">
+          <div class="media-left">
+            <a href="javascript:void(0)">
+              <img style="width: 138px; height: 78px;" :attr="{src: v.poster}">
+            </a>
+          </div>
+          <div class="media-body text-left">
+            <dl>
+              <dt :attr="{title: v.title}">{{v.title}}</dt>
+              <dd>{{v.create_time}}</dd>
+            </dl>
+          </div>
+        </div>
       </td>
-      <td class="tb02">
-        <p class="mar_top60"><i class="icon icon_redio"></i><span class="redtext">36</span></p>
+      <td>
+        <p>{{v.status | statusMsg}}</p>
       </td>
-      <td class="tb02">               
-        <p class="mar_top40"><a href="">添加视频</a></p>
-        <p><a href="">管理节目</a></p>
-        <p><a href="" class="">删除</a></p>
+      <td>
+        <p>
+          <i class="icon icon_redio"></i>
+          <span class="redtext">{{v.plays}}</span>
+        </p>
+        <p>
+          <i class="icon icon_redio"></i>
+          <span class="redtext">{{v.comments}}</span>
+        </p>
+      </td>
+      <td>
+        <p><a href="javascript:void(0)" :click="edit">编辑</a></p>
+        <p><a href="javascript:void(0)" :click="remove">删除</a></p>
       </td>
     </tr>
-    <tr class="list">
-      <td class="tb01">
-        <label class="checkbox" for="checkbox2">
-          <span class="icon-to-fade"></span>
-                <input type="checkbox" value="" id="checkbox2">
-            </label>
-            <img src="img/videolist.png">
-          <span class="listtitle" title="2016年度电影TOP50">2016年度电影TOP50</span>
-          <span class="listdate">2017-03-2315:23</span>
-      </td>
-      <td class="tb02">
-        3
-      </td>
-      <td class="tb02">
-        <p class="mar_top60"><i class="icon icon_redio"></i><span class="redtext">36</span></p>
-      </td>
-      <td class="tb02">               
-        <p class="mar_top40"><a href="">添加视频</a></p>
-        <p><a href="">管理节目</a></p>
-        <p><a href="" class="">删除</a></p>
-      </td>
-    </tr>                   
-  </tbody></table>
-</div>
+  </tbody>
+</table>
 `
+
+let uploadStatus = {
+  1: '初始化',
+  2: '审核通过',
+  3: '发行失败'
+}
 
 export default {
   name: 'k-video-list',
   template,
   data () {
-    return {}
+    return {
+      allChecked: false,
+      list: [{
+        id: '1',
+        title: '我的人生',
+        poster: 'http://img.video.kanimg.kankan.com/section_480x270/9b/6d/9bca949056df5f6a41979130e85304d16e69ce6d_30.jpg',
+        create_time: '2017-03-23 15:23',
+        status: 1,
+        plays: '12312',
+        comments: '100',
+        checked: false
+      }, {
+        id: '2',
+        title: '一条汪汪的故事',
+        poster: 'http://img.video.kanimg.kankan.com/section_480x270/b2/df/b2d3946cfa159e1108a788cca8bd7173f00d68df_30.jpg',
+        create_time: '2017-03-23 15:23',
+        status: 2,
+        plays: '1237772',
+        comments: '353',
+        checked: false
+      }, {
+        id: '3',
+        title: '时代复分',
+        poster: 'http://img.video.kanimg.kankan.com/section_480x270/a0/bc/a0d2fe1a3af5be6779501d8639c6a816b2fd79bc_30.jpg',
+        create_time: '2017-03-23 15:23',
+        status: 3,
+        plays: '1312',
+        comments: '45',
+        checked: false
+      }]
+    }
   },
-  computed: {
-  }
+  methods: {
+    checkAll (evt) {
+      let checked = this.allChecked = !this.allChecked
+      this.list.forEach(function(v) {
+        v.checked = checked
+      })
+    },
+    checkOne (evt, video) {
+      let checked = video.checked = !video.checked
+      if (checked === false) {
+        this.allChecked = false
+      } else {//avalon已经为数组添加了ecma262v5的一些新方法
+        this.allChecked = this.list.every(function (v) {
+          return v.checked
+        })
+      }
+    },
+    edit () {
+
+    },
+    remove () {
+
+    }
+  },
+  filters: {
+    statusMsg (status) {
+      return uploadStatus[status] || '未知错误'
+    }
+  },
 }
